4e9a4394ebd9b97b0b7ef85acfe72c874f9db0d6
bassima_gh <ghba20wu@stident.ju.se>
2022-09-16 23:34:02 +0200

    built the basic HTML for the website

--- app.js
+++ app.js
@@ -3,11 +3,47 @@ const expressHandlebars = require("express-handlebars")
 const app = express()
 
 app.engine("hbs", expressHandlebars.engine({
-    defaultLayout: "human.hbs",
+    defaultLayout: "main.hbs",
 }))
 
-app.get("/humans", function(req, res){
-    res.render("human.hbs")
+app.use (
+    express.static("public")
+)
+
+app.get("/", function(req, res){
+    res.render("start.hbs")
+})
+
+app.get("/about", function(req, res){
+    res.render("about.hbs")
+})
+
+app.get("/contact", function(req, res){
+    res.render("contact.hbs")
+})
+
+app.get("/projects", function(req, res){
+    res.render("projects.hbs")
+})
+
+app.get("/login", function(req, res){
+    res.render("login.hbs")
+})
+
+app.get("/dashboard", function(req, res){
+    res.render("dashboard.hbs", {layout: "admin.hbs"})
+})
+
+app.get("/admin_blog", function(req, res){
+    res.render("admin_blog.hbs", {layout: "admin.hbs"})
+})
+
+app.get("/admin_faq", function(req, res){
+    res.render("admin_faq.hbs", {layout: "admin.hbs"})
+})
+
+app.get("/admin_projects", function(req, res){
+    res.render("admin_projects.hbs", {layout: "admin.hbs"})
 })
 
 
