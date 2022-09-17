c60255927368f21a7bd080361d6135fa06b95910
bassima_gh <ghba20wu@stident.ju.se>
2022-09-18 00:36:36 +0200

    tailwind works perfectly fine

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,hbs}"
+    "./views/**/*.{html,js,hbs}",
+    "./src/**/*.{html,js,hbs}",
+    "./src/**/*.js"
   ],
   theme: {
     extend: {},
