33a8aa58d11cebcbe3eb8b2380948ba8dc9696a2
bassima_gh <ghba20wu@stident.ju.se>
2022-09-23 22:05:16 +0200

    in the process of making the add, edit and remove functions work

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const sqlite3 = require("sqlite3")
 
 const db = new sqlite3.Database("projects_database.db")
 
+const project_name_max_chara = 100
+
 db.run(`
     CREATE TABLE IF NOT EXISTS projects (
         id INTEGER PRIMARY KEY,
@@ -12,13 +14,43 @@ db.run(`
         project_description TEXT
     )`
 )
+// is it gonna make it harder for me if i add foreign keys to different tables?
+// how to add DATE in a database table.
+// how to edit database values inside vscode
+// how to add projects to a database table from the website (i have issues with it maybe because of me having a lot of pages?).
+// is it possible to add a layout and a data object in a render property.
+// can i copy tailwind componenets from the internet (search bar, pagination, etc.).
+
+
+
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS blogs (
+        id INTEGER PRIMARY KEY,
+        post_title TEXT,
+        post_text TEXT,
+        post_date TEXT,
+        FOREIGN KEY (id) REFERENCES projects(id)
+    )`
+)
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS faqs (
+        id INTEGER PRIMARY KEY,
+        post_question TEXT,
+        post_answer TEXT,
+        post_date TEXT
+    )`
+)
 
 const app = express()
 
 //sets the default layout to the main.hbs file
 app.engine("hbs", expressHandlebars.engine({
     defaultLayout: "main.hbs",
+    partialsDir: __dirname + "/views/partials"
 }))
+
 
 app.use (
     express.static("public")
@@ -95,6 +127,29 @@ app.get("/admin_projects", function(req, res){
     res.render("admin_projects.hbs", {layout: "admin.hbs"})
 })
 
+app.post("/projects/add", function(req, res){
+    const project_name = req.body.project_name
+    const project_sub_headline = req.body.project_sub_headline
+    const project_description = req.body.project_description
+
+    const errorMessage = []
+
+    if (project_name == "") {
+        errorMessage.push("name cant be empty")
+    } else if (project_name.length > maxChara) {
+        errorMessage.push("name should be less than " + project_name_max_chara + " charachters")
+    }
+
+    const query = `
+        INSERT INTO projects (project_name, project_sub_headline, project_description) VALUES (?, ?, ?)
+    `
+    const values = [project_name, project_sub_headline, project_description]
+
+    db.run(query, values, function(error){
+        res.redirect("/projects_edit")
+    })
+})
+
 // blog add page
 app.get("/admin_blog", function(req, res){
     res.render("admin_blog.hbs", {layout: "admin.hbs"})
@@ -114,10 +169,11 @@ app.get("/projects_edit", function(req, res){
 
     db.all(query, function(error, projects) {
         const model = {
-            projects 
+            projects ,
+            layout: "admin.hbs"
         }
 
-        res.render("projects.hbs",{layout: "admin.hbs"}, model)
+        res.render("projects_edit.hbs", model)
     }) 
 })
 
