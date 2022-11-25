d54eae3acdd4d86370e143e246b75fc4257a41fa
bassima_gh <ghba20wu@stident.ju.se>
2022-11-26 00:56:27 +0100

    changed some of the theme for the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,13 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				light_body_bg_color: "#FFF7EF",
+				light_body_bg_color: "#121212",
 				light_nav_body_text_color: "#FFF7EF",
-				nav_body_text_color: "#121212",
-				primary_color: "#FF6701",
-				secondary_color: "#1A3545",
+				nav_body_text_color: "#FFF7EF",
+				primary_color: "#86112E",
+				secondary_color: "#15252f",
 				darker_secondary_color: "#152a37",
-				hover_color: "#ff3d01"
+				hover_color: "#800000"
 			},
 			fontSize: {
 				"nav_text": "2.5em"
